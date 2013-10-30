a34bebb3b2938893d1cbca56ee6db24c9f38f65c
Steven Jack <stevenmajack@gmail.com>
2013-10-30 07:54:57 +0000

    Unit tests for setting a value

--- test/Smaj/EtcdTest.php
+++ test/Smaj/EtcdTest.php
@@ -15,4 +15,38 @@ class EctdTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(4001, $ectd->getPort());
     }
 
+    public function testSetAction() {
+        $jsonResponse = '{"action":"SET","key":"/testkey","value":"test","newKey":true,"index":8}';
+        
+        $mockResponse = \Mockery::mock('\Guzzle\Http\Message\Response');
+        $mockResponse
+            ->shouldReceive('json')
+            ->once()
+            ->andReturn($jsonResponse);
+
+        $mockRequest = \Mockery::mock('\Guzzle\Http\Message\Request');
+        $mockRequest
+            ->shouldReceive('send')
+            ->once()
+            ->andReturn($mockResponse);
+
+        $mockClient = \Mockery::mock('\Guzzle\Http\Client');
+        $mockClient
+            ->shouldReceive('post')
+            ->once()
+            ->andReturn($mockRequest);
+
+        $guzzleRequest = new \Smaj\GuzzleRequest($mockClient);
+
+        $ectd = new \Smaj\Etcd('127.0.0.1', 4001);
+        $etcd->setClient($guzzleClient);
+
+        $response = $etcd->set('testkey', 'test');
+
+        $this->assertInstanceOf('\Smaj\Response', $response);
+        $this->assertEquals('testkey', $response->getKey());
+        $this->assertEquals('test', $response->getValue());
+
+    }
+
 }
